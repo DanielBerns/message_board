6883a403d5914d9666f4f09df5da7eb12182585c
DanielBerns <daniel.w.berns@gmail.com>
2025-06-02 17:47:36 -0300

    fixing deploy

--- src/server/run.py
+++ src/server/run.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-from app import create_app
+from application import create_app
 
 # Create the Flask app instance using the app factory
 # It will look for FLASK_CONFIG in environment variables, defaulting to 'development'
